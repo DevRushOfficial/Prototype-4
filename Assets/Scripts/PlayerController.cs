2505b4cfcb82dc0162d3c684233a6233eda333fc
Artem Melnykov <artemmelnykov29@gmail.com>
2024-09-17 16:50:29 +0200

    Allow player move in direction of focal point

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,18 @@ public class PlayerController : MonoBehaviour
 {
     private float _speedRolling = 5.0f;
     private Rigidbody _playerRb;
+    private GameObject _focalPoint;
 
-    private void Start() => _playerRb = GetComponent<Rigidbody>();
+    private void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
+    }
 
     void Update()
     {
         float forwardInput = Input.GetAxis("Vertical");
 
-        _playerRb.AddForce(Vector3.forward * forwardInput * _speedRolling);
+        _playerRb.AddForce(_focalPoint.transform.forward * forwardInput * _speedRolling);
     }
 }
