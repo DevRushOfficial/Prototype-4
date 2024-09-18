5d43dca870184224f0b11a039464efa92fe37f51
Artem Melnykov <artemmelnykov29@gmail.com>
2024-09-18 20:34:55 +0200

    Refactored script

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,11 +8,16 @@ public class SpawnManager : MonoBehaviour
 
     void Start()
     {
+        Instantiate(_enemyPrefab, GenerateSpawnPosition(), _enemyPrefab.transform.rotation);
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
         float spawnPositionX = Random.Range(-_spawnRange, _spawnRange);
         float spawnPositionZ = Random.Range(-_spawnRange, _spawnRange);
 
         Vector3 randomPosition = new Vector3(spawnPositionX, 0, spawnPositionZ);
 
-        Instantiate(_enemyPrefab, randomPosition, _enemyPrefab.transform.rotation);
+        return randomPosition;
     }
 }
